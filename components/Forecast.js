8063c3ad200d05e4725015b6b034dd9a3f1b5897
6210110513 <6210110513@psu.ac.th>
2021-07-18 22:24:50 +0700

    Work commit 2

--- components/Forecast.js
+++ components/Forecast.js
@@ -1,16 +1,37 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function Forecast(props) {
     return (
         <View>
-            <Text>{props.main}</Text>
-            <Text>{props.description}</Text>
+            <Text style={styles.innerText2}>{props.main}</Text>
+            <Text style={styles.innerText}>{props.description}</Text>
             <View>
-                <Text>{props.temp}</Text>
+                <Text style={styles.innerText}>{props.temp}</Text>
                 <Text>°C</Text>
             </View>
         </View>
     );
 }
-   
+
+const styles = StyleSheet.create({
+    innerText: {
+        color: 'white',
+        textAlign: 'center',
+        fontSize: 23,
+        paddingBottom: 15,
+        textShadowOffset: {width: 2,height: 2},
+        textShadowRadius: 10,
+        textShadowColor:'black',
+    },
+    innerText2: {
+        color: 'white',
+        textAlign: 'center',
+        fontSize: 35,
+        paddingBottom: 15,
+        textShadowOffset: {width: 2,height: 2},
+        textShadowRadius: 10,
+        textShadowColor:'black',
+    },
+
+   });
